3c6c9d255d4b4ff2e40ce9a8871364893fda3b35
Jonathan Choi <jisung594@gmail.com>
2020-09-20 13:34:33 -0400

    working out S3 upload issue

--- __init__.py
+++ __init__.py
@@ -1,44 +1,63 @@
 from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 from .helpers import *
+from flask_cors import CORS
 
 app = Flask(__name__)
 # -- in terminal --
-# export FLASK_APP=main.py
-# flask run
+# source venv/bin/activate
+# export FLASK_APP=__init__.py
+# python -m flask run
 
 app.config.from_object("config")
+CORS(app)
 
 
-
-# @app.route('/')
-# def index():
-#     return render_template('/index.html')
-
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if "input-file" not in request.files:
-        return "Please upload a file."
-
-    graph_name = request.form["graph-name"]
-    x_axis = request.form["x-axis"]
-    y_axis = request.form["y-axis"]
-    file = request.files["input-file"]
+@app.route("/")
+def index():
+    return render_template("/index.html")
 
 
-    if file.filename == "":
-        return "Please upload a file."
+@app.route("/upload", methods=["POST"])
+def upload():
+    bucket_dir = request.form["bucketDir"]
+    format = request.form["format"]
+    input_file = request.files["inputFile"]
 
+    if input_file and allowed_file(input_file.filename):
+        input_file.filename = secure_filename(input_file.filename)
+        output = upload_to_s3(input_file, app.config["S3_BUCKET"], bucket_dir)
 
-    if file and allowed_file(file.filename):
-        file.filename = secure_filename(file.filename)
-        output = upload_to_s3(file, app.config["S3_BUCKET"], bucket_dir)
-
-        return redirect('/')
+        return redirect("/")
     else:
-        return redirect('/')
+        return redirect("/")
+
+
+
+# def upload_file():
+#     if "input-file" not in request.files:
+#         return "Please upload a file."
+#
+#     graph_name = request.form["graph-name"]
+#     x_axis = request.form["x-axis"]
+#     y_axis = request.form["y-axis"]
+#     file = request.files["input-file"]
+#
+#
+#     if file.filename == "":
+#         return "Please upload a file."
+#
+#
+#     if file and allowed_file(file.filename):
+#         file.filename = secure_filename(file.filename)
+#         output = upload_to_s3(file, app.config["S3_BUCKET"], bucket_dir)
+#
+#         return redirect('/')
+#     else:
+#         return redirect('/')
+
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(Debug=True)
